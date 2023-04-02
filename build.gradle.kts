99ccceb812784ed3b951a9b8eb1a2bda41a7305e
冰凝 <liuli.dnn@email.cn>
2023-04-02 14:03:58 +0800

    ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,46 +4,68 @@ plugins {
     java
 }
 
-group = "cn.template"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "cn.template"
+    version = "1.0-SNAPSHOT"
+}
 
-java {
-    toolchain {
-        // 工具链语言版本必须和 运行 gradle 所用 JDK 版本一致
-        languageVersion.set(JavaLanguageVersion.of(17))
+subprojects {
+    java {
+        toolchain {
+            // 工具链语言版本必须和 运行 gradle 所用 JDK 版本一致
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+        sourceSets {
+            main {
+                java { setSrcDirs(listOf("java")) }
+                resources { setSrcDirs(listOf("resources")) }
+            }
+            test {
+                java { setSrcDirs(listOf("test")) }
+                resources { setSrcDirs(listOf("testResources")) }
+            }
+        }
+        withSourcesJar()
+        withJavadocJar()
     }
 
-    sourceSets {
-        main {
-            java { setSrcDirs(listOf("java")) }
-            resources { setSrcDirs(listOf("resources")) }
-        }
-        test {
-            java { setSrcDirs(listOf("test")) }
-            resources { setSrcDirs(listOf("testResources")) }
+    configurations.all {
+        resolutionStrategy.cacheDynamicVersionsFor(1, TimeUnit.DAYS)
+        resolutionStrategy.cacheChangingModulesFor(1, TimeUnit.DAYS)
+    }
+
+    dependencies {
+        implementation(libs.slf4j)
+        implementation(libs.slf4jJul)
+        runtimeOnly(libs.logback)
+
+        compileOnly(libs.lombok)
+        annotationProcessor(libs.lombok)
+        testCompileOnly(libs.lombok)
+        testAnnotationProcessor(libs.lombok)
+
+        testImplementation(testLibs.junitApi)
+        testRuntimeOnly(testLibs.junitEngine)
+        testRuntimeOnly(testLibs.junitPlatformLauncher)
+    }
+
+    tasks.getByName<Test>("test") {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = StandardCharsets.UTF_8.name()
+    }
+
+    @Suppress("SpellCheckingInspection")
+    tasks.withType<Javadoc> {
+        options {
+            this as StandardJavadocDocletOptions
+            encoding = StandardCharsets.UTF_8.name()
+            charset(StandardCharsets.UTF_8.name())
+            addBooleanOption("Xdoclint:none", true)
+            addStringOption("Xmaxwarns", "1")
         }
     }
-}
 
-dependencies {
-    implementation(libs.slf4j)
-    implementation(libs.slf4jJul)
-    runtimeOnly(libs.logback)
-
-    compileOnly(libs.lombok)
-    annotationProcessor(libs.lombok)
-    testCompileOnly(libs.lombok)
-    testAnnotationProcessor(libs.lombok)
-
-    testImplementation(testLibs.junitApi)
-    testRuntimeOnly(testLibs.junitEngine)
-    testRuntimeOnly(testLibs.junitPlatformLauncher)
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
-
-tasks.withType<JavaCompile> {
-    options.encoding = StandardCharsets.UTF_8.name()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,31 +2,62 @@
 
 rootProject.name = "java-gradle-template"
 
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/spring-plugin")
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
+        val aliYun: (PasswordCredentials).() -> Unit = {
+            username = providers.gradleProperty("MAVEN-REPO-AL-USERNAME").getOrElse("")
+            password = providers.gradleProperty("MAVEN-REPO-AL-PASSWORD").getOrElse("")
+        }
+        maven {
+            name = "阿里云效仓库"
+            setUrl("https://packages.aliyun.com/maven/repository/2312424-release-8x1TrK/")
+            credentials(aliYun)
+            mavenContent { releasesOnly() }
+        }
+        maven {
+            name = "阿里云效镜像仓库"
+            setUrl("https://packages.aliyun.com/maven/repository/2312424-snapshot-SeVvm7/")
+            credentials(aliYun)
+            mavenContent { snapshotsOnly() }
+        }
         maven("https://maven.aliyun.com/repository/public/")
+        maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/spring/")
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
         mavenCentral()
     }
 
+    @Suppress("SpellCheckingInspection")
     versionCatalogs {
         create("libs") {
-            @Suppress("SpellCheckingInspection")
-            library("lombok", "org.projectlombok:lombok:1.18.26")
             library("jetbrainsAnnotations", "org.jetbrains:annotations:24.0.0")
+            library("lombok", "org.projectlombok:lombok:1.18.26")
             library("slf4j", "org.slf4j:slf4j-api:2.0.6")
             library("slf4jJul", "org.slf4j:jul-to-slf4j:2.0.5")
             library("logback", "ch.qos.logback:logback-classic:1.4.5")
         }
-
-        // 测试环境依赖
+        create("jakarta") {
+            library("inject", "jakarta.inject:jakarta.inject-api:2.0.1")
+            library("annotationApi", "jakarta.annotation:jakarta.annotation-api:2.1.1")
+            library("validationApi", "jakarta.validation:jakarta.validation-api:3.0.2")
+            library("servletApi", "jakarta.servlet:jakarta.servlet-api:6.0.0")
+            library("transactionApi", "jakarta.transaction:jakarta.transaction-api:2.0.1")
+        }
         create("testLibs") {
-            version("junitVersion", "5.9.2")
-            library("junitApi", "org.junit.jupiter", "junit-jupiter-api").versionRef("junitVersion")
-            library("junitEngine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junitVersion")
+            val junitVersion = "5.9.2"
+            library("junitApi", "org.junit.jupiter:junit-jupiter-api:${junitVersion}")
+            library("junitEngine", "org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
             library("junitPlatformLauncher", "org.junit.platform:junit-platform-launcher:1.9.2")
         }
     }
+
 }
